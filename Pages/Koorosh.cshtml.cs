cf1452c2b07bbd7c3bb6c46ebbff3a0a2b00cc6e
Koorosh Dadsetan <Koorosh Dadsetan@DESKTOP-90OC7A4>
2023-01-10 20:42:45 +0330

    Add project files.

---
+++ Pages/Koorosh.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_Project.Pages
+{
+    public class KooroshModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
