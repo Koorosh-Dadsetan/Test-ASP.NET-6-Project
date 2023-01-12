a3805d26112704583e2eb042481e79420ac1dcb8
Koorosh Dadsetan <kooroshdadsetan@outlook.com>
2023-01-12 14:19:22 +0330

    add NewTable page

---
+++ Pages/NewTable.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Test_Project.Pages
+{
+    public class NewTableModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
