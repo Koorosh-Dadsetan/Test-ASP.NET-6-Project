74cf6944750ac24e86f80ccc8a082ce0938918d9
Koorosh Dadsetan <kooroshdadsetan@outlook.com>
2023-01-21 18:12:24 +0330

    7

--- Pages/sqlDataAdapter.cshtml.cs
+++ Pages/sqlDataAdapter.cshtml.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Syncfusion.XlsIO;
+
 
 namespace Test_Project.Pages
 {
@@ -24,6 +26,7 @@ namespace Test_Project.Pages
 
         [BindProperty]
         public int PageSize { get; set; }
+
 
         public void OnGet(int p = 1, int s = 5)
         {
@@ -60,5 +63,80 @@ namespace Test_Project.Pages
 
             PageSize = s;
         }
+
+
+        public IActionResult OnGetMyOnClick(int p = 1, int s = 5)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                try
+                {
+                    conn.Open();
+
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dataTable);
+
+                    conn.Close();
+                    da.Dispose();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+
+            var results = from myRow in dataTable.AsEnumerable()
+                          select myRow;
+
+
+            Cultures = results
+                .Skip((p - 1) * s).Take(s);
+
+            TotalRecords = results.Count();
+
+            PageNo = p;
+
+            PageSize = s;
+
+
+
+
+            using (ExcelEngine excelEngine = new ExcelEngine())
+            {
+                IApplication application = excelEngine.Excel;
+                application.DefaultVersion = ExcelVersion.Excel2016;
+
+                //Create a new workbook
+                IWorkbook workbook = application.Workbooks.Create(1);
+                IWorksheet sheet = workbook.Worksheets[0];
+
+
+
+                //Import data from the data table with column header, at first row and first column, 
+                //and by its column type.
+                sheet.ImportDataTable(dataTable, true, 1, 1, true);
+
+                //Creating Excel table or list object and apply style to the table
+                IListObject table = sheet.ListObjects.Create("Employee_PersonalDetails", sheet.UsedRange);
+
+                table.BuiltInTableStyle = TableBuiltInStyles.TableStyleMedium14;
+
+                //Autofit the columns
+                sheet.UsedRange.AutofitColumns();
+
+                //Save the file in the given path
+                var fileStream = new FileStream(@"E:\Output.xlsx", FileMode.Create);
+                workbook.SaveAs(fileStream);
+                fileStream.Dispose();
+            }
+
+            TempData["msg"] = "Excel file downloaded sucesfully.";
+
+            return Redirect("https://localhost:7109/sqldataadapter");
+
+        }
+
     }
 }
